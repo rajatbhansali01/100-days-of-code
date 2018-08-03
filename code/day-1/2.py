4d7bcad92842eeb3802f5ad15b2a1c722772cce2
Rajat Bhansali <rajatbhansali01@gmail.com>
2018-08-03 23:51:40 +0530

    Day-0-100-Days-of-code

---
+++ code/day-1/2.py
@@ -0,0 +1,7 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> name=input("enter the name")
+enter the name rajat
+>>> print("Hello"+name+ "hope you are enjoy this week")
+Hello rajathope you are enjoy this week
+>>> 
---
+++ code/day-1/3.py
@@ -0,0 +1,11 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print(2+4)
+6
+>>> print(5/2)
+2.5
+>>> print(4*(3*2))
+24
+>>> print(4*3*10)
+120
+>>> 
