05bafb3d4ce3081af4fc7e7b8300419be86e6e8a
Rajat Bhansali <rajatbhansali01@gmail.com>
2018-08-09 23:17:22 +0530

    day-7-100-days-of-code

---
+++ code/day-7/dic1.py
@@ -0,0 +1,7 @@
+data1={'Id':100, 'Name':'Rajat', 'Profession':'Developer'}  
+data2={'Id':101, 'Name':'Shubham', 'Profession':'Trainer'}  
+data1['Profession']='Project Manager'  
+data2['Salary']=20000  
+data1['Salary']=15000  
+print (data1)
+print (data2)  
