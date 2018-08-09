add5ed76ae589c06d1d0d2b6175b3e43055a4ad1
Rajat Bhansali <rajatbhansali01@gmail.com>
2018-08-09 23:18:41 +0530

    day-7-100-days-of-code

---
+++ code/day-7/dictionry.py
@@ -0,0 +1,6 @@
+data1={'Id':1037, 'Name':'Rajat', 'Profession':'Software Developer'}  
+data2={'Id':14036, 'Name':'Bhansali', 'Profession':'Web Developr'}  
+print ("Id of 1st employer is",data1['Id'])
+print ("Id of 2nd employer is",data2['Id'])
+print ("Name of 1st employer:",data1['Name']) 
+print ("Profession of 2nd employer:",data2['Profession'])
