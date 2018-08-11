ed36720a35fcd1ee3c7f995983da1133b665a472
Rajat Bhansali <rajatbhansali01@gmail.com>
2018-08-11 23:42:45 +0530

    day-9-100-days-of-code

---
+++ code/day-9/codecourse.py
@@ -0,0 +1,16 @@
+print("enter ur course code")
+x=(input());
+if x=='mca':
+    print("user is mca");
+elif x=='mba':
+    print("Student is mba");
+elif x=='as':
+    print("Student is Applied Science");
+elif x=='sop':
+    print("Student is school of pharmacy");
+elif x=='cs':
+    print("student is computer science");
+elif x=='it':
+    print("student is Information technology");
+else:
+    print("another course")
